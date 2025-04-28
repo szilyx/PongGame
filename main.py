1e837345b47e7afffa39b84c28ce5670a686dcb3
szilyx <forgacsszilard1@gmail.com>
2025-04-28 15:55:58 +0300

    Scoreboard and speed upadte, final project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 MainScreen = Screen()
@@ -14,6 +15,7 @@ MainScreen.listen()
 
 paddle_1 = Paddle(350,0)
 paddle_2 = Paddle(-350,0)
+scoreboard = Scoreboard()
 ball = Ball()
 MainScreen.onkey(paddle_1.moveup, "Up")
 MainScreen.onkey(paddle_1.movedown, "Down")
@@ -22,8 +24,25 @@ MainScreen.onkey(paddle_2.movedown, "s")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     MainScreen.update()
     ball.start()
+
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(paddle_1) < 50 and ball.xcor() > 320 or ball.distance(paddle_2) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.restargame()
+        scoreboard.l_score += 1
+        scoreboard.refresh_board()
+
+    if ball.xcor() < -380:
+        ball.restargame()
+        scoreboard.r_score += 1
+        scoreboard.refresh_board()
 
 MainScreen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.goto(-100,200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100,200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+
+    def refresh_board(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
